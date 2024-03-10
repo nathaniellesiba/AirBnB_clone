fe317e5159f8c0ae0d24d36f6f72c423b94a41c7
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-10 23:03:30 +0200

    quitpy

--- models/console.py
+++ models/console.py
@@ -2,32 +2,23 @@
 
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """This class implements a simple CRUD command interpreter."""
+    """Command interpreter for HBNB project"""
 
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
-        """Exit the command interpreter when EOF is reached."""
+        """Exit the command interpreter when EOF is reached"""
         return True
-
-    def do_default(self, line):
-        """Handle unknown commands."""
-        print(f"Unknown command: {line}")
-
-    def help_welcoming(self):
-        """Display help for the 'welcoming' command."""
-        print("Usage: welcoming [person]")
-
-    def emptyline(self):
-        """Do nothing when an empty line is entered."""
-        pass
 
     def do_quit(self, line):
-        """Exit the command interpreter."""
+        """Exit the command interpreter"""
         return True
 
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    prompt = HBNBCommand()
+    prompt.cmdloop()
