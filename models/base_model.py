bd0304ac08f4653928eed42bf77adc3b780285d6
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-10 23:18:38 +0200

    base

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 
-import filestorage
+import uuid
+from datetime import datetime
 
-class BaseModel():
-    """define common attributes
-    across all classes and define
-    common methods across all classes
-    """
+class BaseModel:
+    """Defines common attributes and methods for other classes"""
+
+    def __init__(self):
+        """Initialize instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        """calling a save method"""
+        """Update the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
 
-
-    if __name__ == '__main__':
-        filestorage.FileStorage()
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of __dict__ of the instance"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
