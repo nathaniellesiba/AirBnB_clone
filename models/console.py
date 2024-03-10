47155ccd190278c8b4485b89e8a2e966e5070cb2
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-10 22:59:31 +0200

    pycode

--- models/console.py
+++ models/console.py
@@ -2,33 +2,32 @@
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    """is a simple CRUD command
-    for EOF, quiting, prompt, help"""
+    """This class implements a simple CRUD command interpreter."""
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
-    def do_EOF(seld, line):
-        """this execute end of file"""
-        print()
+    def do_EOF(self, line):
+        """Exit the command interpreter when EOF is reached."""
         return True
 
     def do_default(self, line):
-        print(f"unknown command; {line}")
+        """Handle unknown commands."""
+        print(f"Unknown command: {line}")
 
-    def help welcoming(self):
-        """this is a customized or
-        detailed help"""
-        print('/n'.join([
-            'welcoming [person]']))
+    def help_welcoming(self):
+        """Display help for the 'welcoming' command."""
+        print("Usage: welcoming [person]")
 
     def emptyline(self):
-        """this is printing empty
-        line notification"""
-        print("you have entered empty line")
+        """Do nothing when an empty line is entered."""
+        pass
 
     def do_quit(self, line):
-        return True 
+        """Exit the command interpreter."""
+        return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
