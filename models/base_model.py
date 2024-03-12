f60e284ef57f7b3b0191475ea65cc791a093a158
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-13 00:34:19 +0200

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,23 @@ from datetime import datetime
 class BaseModel:
     """Defines common attributes and methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs_date = strftime('%y-%m-%dT%H%M%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     kwargs_date)
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     kwargs_date)
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
+
 
     def __str__(self):
         """Return a string representation of the instance"""
@@ -19,7 +31,9 @@ class BaseModel:
 
     def save(self):
         """Update public instance attr updated_at with current datetime"""
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save
 
     def to_dict(self):
         """Return dictionary containing all keys/values
