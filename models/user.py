e37f3532bbc52e5fcdbc590c4464bc978365aa23
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-13 01:29:16 +0200

    amenities

--- models/user.py
+++ models/user.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
+"""defining a user"""
 from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
-
-    def __init__(self):
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
